07aafe548654623bdc942033eaed1a207639eccd
Jeong tae yeon <jtyk119@naver.com>
2023-10-12 16:36:26 +0900

    Drill09 제출 (2022182037 정태연)

--- boy.py
+++ boy.py
@@ -58,7 +58,7 @@ class AutoRun:
 
     @staticmethod
     def draw(boy):
-        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y+20, 150, 150)
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y + 20, 150, 150)
 
 
 class Run:
